5e3b57c37d5d385a86b9fcd183c0a8e193e2f325
Aparna5101 <111563562+Aparna5101@users.noreply.github.com>
2022-11-17 09:45:49 +0530

    Add files via upload

---
+++ src/card.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './'
+
+function Card() {
+  return (
+    <>
+    <div id="container">
+    <h2>CAKE SHOP</h2>
+<div class="card">
+  <img src="https://jelajah-indonesia.co.id/wp-content/uploads/2021/01/The-Mandarin-Cake-Shop-Interior-1.jpg" alt="Lago di Braies"></img>
+
+  
+  <div class="card__details">
+
+    
+    <span class="tag">Cakes</span>
+
+    <span class="tag">Deserts</span>
+
+    
+    <div class="name">CAKE SHOP</div>
+
+    <p>We at The Cake Shop specialize in Desserts & Cakes. we believe in "Quality in our Edge", from selecting the ingredients around India to the final exqusite creation that "Bring you smiles for Sure"
+
+Our cakes & Desserts receive a lot of compliments and are complimented by wide range of exclusive desserts.
+
+Our theme based Creative Cakes, Cake pop , Cup Cakes and Desserts for Parities, make you Party Unique and Inevitable. We spend more time in understanding customers expectation.
+</p>
+
+    <button>Read more</button>
+  </div>
+
+
+</div>
+</div>
+
+    </>
+  );
+}
+
+export default Card;
